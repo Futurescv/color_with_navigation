8a2b62a6bda309a8204ae2db00997104dc50eaa3
123 <a123@123deMacBook.local>
2018-10-16 20:59:42 -0500

    JinWu

---
+++ Colors/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by 123 on 2018/10/16.
+//  Copyright © 2018年 Jin. All rights reserved.
+//
+import  UIKit
+import Foundation
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
